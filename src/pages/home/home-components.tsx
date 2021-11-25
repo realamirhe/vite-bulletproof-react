fdf2236ac47f1fc13b25cc8d9b851d6be796e96f
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-11-25 16:33:10 +0330

    fix: Prevent passing props to inner component
    - Fix styling issue

--- src/pages/home/home-components.tsx
+++ src/pages/home/home-components.tsx
@@ -3,12 +3,15 @@ import { styled } from "@mui/system";
 
 type MyTypographyExtraProps = { isEven?: boolean };
 
-export const MyTypography = styled(MuiTypography)<MyTypographyExtraProps>(
+export const MyTypography = styled(MuiTypography, {
+  shouldForwardProp: prop => prop !== "isEven",
+})<MyTypographyExtraProps>(
   ({ theme, isEven }) => `
    color: ${theme.palette.common.black};
 
    ::after {
       content: "";
+      display: inline-block;
       height: ${theme.spacing(2)};
       width: ${theme.spacing(2)};
       border-radius: ${theme.spacing(2)};
